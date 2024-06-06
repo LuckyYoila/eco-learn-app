b3a0f22a55295cdff841817ea715e937759abbb9
LuckyYoila <luckyyoila626@gmail.com>
2024-06-06 12:50:23 +0100

    updated tailwind config colors and dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -11,20 +11,20 @@ export const metadata: Metadata = {
   description: "Learn about the environment and how you can help",
 };
 
-export default function RootLayout({
+export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} bg-gray-50 relative p-0 m-0 min-h-screen `}>
+    <div
+      className={`${inter.className} bg-gray-50 relative p-0 m-0 min-h-screen `}
+    >
       <SideNav />
       <TopBar />
       <div className="ml-[18%] min-h-screen px-6 pt-[6%] bg-gray-50">
         {children}
       </div>
-      </body>
-    </html>
+    </div>
   );
 }
